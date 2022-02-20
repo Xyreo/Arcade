e0dce64fcb405744676da271fb095fc1fb858e18
Chaitanya-Keyal <66475772+Chaitanya-Keyal@users.noreply.github.com>
2022-02-20 11:10:53 +0530

    gui start

---
+++ Monopoly.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import font
+import tkinter.ttk as ttk
+from PIL import ImageTk, Image, ImageOps
+
+root = tk.Tk()
+root.title("Monopoly")
+root.geometry("1250x800")
+
+s=ttk.Style()
+s.configure('my.TButton',font=("Helvetica",30))
+   
+boardIMG = ImageTk.PhotoImage(
+    ImageOps.expand(
+        Image.open("Images/BoardIMG.jpg"),border=3).resize((750, 750)))
+
+canvas = tk.Canvas(root, width=750, height=750) 
+canvas.place(x=10,y=25)
+canvas.create_image(2,2,image=boardIMG,anchor='nw')
+
+but = tk.Button(canvas, text="DEATH").place(x=287.5,y=349,width=175,height=52)
+
+
+root.mainloop()
