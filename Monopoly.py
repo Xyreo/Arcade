aa10ae6627e54a88229bbf0e5cec968b9f08f8cd
Chaitanya-Keyal <66475772+Chaitanya-Keyal@users.noreply.github.com>
2022-02-20 22:39:36 +0530

    tkinter should die

--- Monopoly.py
+++ Monopoly.py
@@ -1,21 +1,35 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageOps
 
 root = tk.Tk()
 root.title("Monopoly")
-root.geometry("1250x800")
+root.wm_attributes('-transparentcolor', '#1b6e81')
+
+screenwidth = root.winfo_screenwidth()-14
+screenheight = root.winfo_screenheight()-44
+root.geometry(f"{screenwidth}x{screenheight}")
 
 s=ttk.Style()
-s.configure('my.TButton',font=("Helvetica",30))
-   
-boardIMG = ImageTk.PhotoImage(Image.open("Images/BoardIMG.jpg"))
+s.configure('my.TButton',font=("Helvetica",20))
 
-canvas = tk.Canvas(root, width=750, height=750) 
-canvas.place(x=10,y=25)
+boardside = screenheight-65
+a = boardside/12.2
+
+boardIMG = ImageTk.PhotoImage(
+    ImageOps.expand(
+        Image.open("Images/BoardIMG.jpg").resize((boardside,boardside),Image.ANTIALIAS)))
+
+canvas = tk.Canvas(root, width=boardside, height=boardside) 
+canvas.place(x=10,y=32.5)
 canvas.create_image(2,2,image=boardIMG,anchor='nw')
 
-but = ttk.Button(canvas, text="Roll Dice",style="my.TButton").place(x=287.5,y=349,width=175,height=52)
+roll = ttk.Button(canvas, text="Roll Dice",style="my.TButton")
+roll.place(x=boardside/2 - roll.winfo_width() ,y=boardside/2 - roll.winfo_height(),anchor='center')
+
+passgo = tk.Button(canvas, text='hi',bg='#1b6e81')
+passgo.place(x=boardside - (a+0.6*a) ,y=boardside-(a+0.6*a), width=a+0.6*a, height=a+0.6*a)
 
 
 root.mainloop()
+ 
