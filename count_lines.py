592cf02527f1bc585ed835fac8238330a5c7e1b7
Chaitanya-Keyal <66475772+Chaitanya-Keyal@users.noreply.github.com>
2022-07-27 01:44:31 +0530

    Count Lines

--- count_lines.py
+++ count_lines.py
@@ -9,5 +9,8 @@ print(
         a,
         dict(zip(a[0].keys(), [i[0].upper() + i[1:] for i in a[0].keys()])),
         "fancy_grid",
+        stralign="center",
+        numalign="right",
+        showindex=list(range(1, len(a))) + [None],
     )
 )
