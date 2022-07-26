a50ee2842294c1368008eb8300761a86eb71ae7d
Chaitanya-Keyal <66475772+Chaitanya-Keyal@users.noreply.github.com>
2022-07-27 01:29:15 +0530

    Count Lines

---
+++ count_lines.py
@@ -0,0 +1,13 @@
+import requests
+from tabulate import tabulate as tab
+
+a = requests.get(
+    "https://api.codetabs.com/v1/loc?github=Chaitanya-Keyal/Arcade&ignored=.github,LICENSE,.gitignore"
+).json()
+print(
+    tab(
+        a,
+        dict(zip(a[0].keys(), [i[0].upper() + i[1:] for i in a[0].keys()])),
+        "fancy_grid",
+    )
+)
